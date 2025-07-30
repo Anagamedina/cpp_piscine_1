a2184c2c08e51923a05a77e6579e94cfd1116c2a
Anagamedina <anaga.medina@gmail.com>
2025-07-30 17:44:36 +0200

    tabla completada con 10 caracteres

--- ex01/PhoneBook.cpp
+++ ex01/PhoneBook.cpp
@@ -86,84 +86,58 @@ void PhoneBook::add(void)
 }
 
 
-//imprimir todos los contactos
-//---->>> void print() setw
-//----->>> truncar con (.)
-//pregnutar que indice quieres ver
-//comprobar que el indice exista y si no mensaje error
-//comprobar que el indice exista, entregar datos del contacto x
 
 
-/*void PhoneBook::searchContact(int new_index)
-{
-	size_t contact_count;
-
-	if (_index > 8)
-		contact_count = 8;
-	else
-		contact_count = _index;
-
-	if (new_index < 0 || new_index >= (int)contact_count)
-	{
-		std::cout << "❌ Invalid index.\n";
-		return;
-	}
-
-	Contact contact = _contacts[new_index];
-
-	std::cout << "First name:     " << contact.get_name() << std::endl;
-	std::cout << "Last name:      " << contact.get_last_name() << std::endl;
-	std::cout << "Nickname:       " << contact.get_nickname() << std::endl;
-	std::cout << "Phone number:   " << contact.get_phone() << std::endl;
-	std::cout << "Darkest secret: " << contact.get_secret() << std::endl;
-}*/
 void PhoneBook::print_table(void)
 {
-		std::cout << std::setw(10) << "Index" << "|"
-				<< std::setw(10) << "Name" << "|"
-				<< std::setw(10) << "LastName" << "|"
-				<< std::setw(10) << "Nickname" << std::endl;
-
-	for (size_t i = 0; i < this->_max_size; i++)
+	if (this->_index == 0)
 	{
-		std::cout << std::setw(10) << i << "|";
+		std::cout << "Agenda vacia: Añade un contacto primero.\n";
+		return ;
+	}
+	std::cout << "|" << std::setw(10) << "Index" << "|"
+			<< std::setw(10) << "Name" << "|"
+			<< std::setw(10) << "LastName" << "|"
+			<< std::setw(10) << "Nickname" << "|" << std::endl;
+
+	int max_index;
+	if (this->_index == 8)
+		max_index = 8;
+	else
+		max_index = this->_index;
+
+	for (int  i = 0; i < max_index; i++)
+	{
+		std::cout << "|" << std::setw(10) << i << "|";
 		print_columns(_contacts[i].get_name());
 		std::cout << "|";
 		print_columns(_contacts[i].get_last_name());
 		std::cout << "|";
 		print_columns(_contacts[i].get_nickname());
-		std::cout << std::endl;
+		std::cout  << "|" << std::endl;
 	}
 }
 
 void PhoneBook::searchContact(int index)
 {
-	size_t	max_index;
+	int 	max_index;
 
-	if (_index == 0)
+	if (this->_index == 0) //cambio this->
 	{
 		std::cout << "Agenda vacia: Añade un contacto primero.\n";
 		return ;
 	}
-
-	if (_index == 8)
-		max_index = 8;
-	else
-		max_index = _index;
-
-
+	if (this->_index == 8)
+			max_index = 8;
+		else
+			max_index = this->_index;
 	
-	//pedir indice usuario 
-	std::cout << "Add index of contact you want to see: ";
-	//int index;
-	//std::cin >> index;
-
-	if (index < 0 )//|| index >= this->_index)
-	{
-		std::cout << "Index out of range\n";
-		return ;
-	}
-
+		std::cout << "Add index of contact you want to see: ";
+		if (index < 0 || index >= max_index)
+		{
+			std::cout << "Index out of range\n";
+			return ;
+		}	
 	//Mostrar datos del contacto elegido
 	std::cout << std::setw(10) << "Name" << ": " << _contacts[index].get_name() << std::endl;
 	std::cout << std::setw(10) << "Last name" << ": " << _contacts[index].get_last_name() << std::endl;
@@ -172,6 +146,5 @@ void PhoneBook::searchContact(int index)
 	std::cout << std::setw(10) << "Secret" << ": " << _contacts[index].get_secret() << std::endl;
 
 }
-
 
 	
