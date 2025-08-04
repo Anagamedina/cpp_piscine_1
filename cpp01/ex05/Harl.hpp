2140a7a32274874ad7d7d786c56ccc1276c528b6
Anagamedina <anaga.medina@gmail.com>
2025-08-04 14:01:23 +0200

    makefile ex05

---
+++ cpp01/ex05/Harl.hpp
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anamedin <anamedin@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/04 13:05:25 by anamedin          #+#    #+#             */
+/*   Updated: 2025/08/04 14:01:01 by anamedin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HARL_HPP
+# define HARL_HPP
+
+#include <iostream>
+#include <string>
+
+class Harl
+{
+  private:
+    //funciiones  estaticas 
+    void  _debug( void );
+    void  _info( void );
+    void  _warning( void );
+    void  _error( void );
+
+
+  public:
+
+    Harl();
+    ~Harl();
+    void  complain(std::string level );
+
+}
+
+
+#endif;
