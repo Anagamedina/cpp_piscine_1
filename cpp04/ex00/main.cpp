71c944cf90c388d794336e56cc3bc7d86ed71945
Anagamedina <anaga.medina@gmail.com>
2025-08-13 13:49:25 +0200

    add ex00

---
+++ cpp04/ex00/main.cpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anamedin <anamedin@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/13 12:34:35 by anamedin          #+#    #+#             */
+/*   Updated: 2025/08/13 13:04:19 by anamedin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Animal.hpp"
+#include "Dog.hpp"
+#include "Cat.hpp"
+#include "WrongAnimal.hpp"
+#include "WrongCat.hpp"
+
+int main (void ){
+  
+  const Animal* meta = new Animal();
+  const Animal* j = new Dog();
+  const Animal* i = new Cat();
+
+  std::cout << j->getType() << " " << std::endl;
+  std::cout << i->getType() << " " << std::endl;
+  i->makeSound();
+  j->makeSound();
+  meta->makeSound();
+
+
+
+//....
+  return  (0);
+}
+
